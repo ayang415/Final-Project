3a5c5d95db2a4bfe4a167eeccd10bfd89b0507b6
ayang415 <148258723+ayang415@users.noreply.github.com>
2023-12-11 11:16:31 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,13 +2,23 @@ import java.util.ArrayList;
 
 public class Player {
     private ArrayList<Character> hand = new ArrayList<Character>();
+    private ArrayList<String> preppedTiles = new ArrayList<String>();
     private int points = 0;
+    private boolean win = false;
 
     public ArrayList<Character> getHand() {
         return hand;
     }
 
+    public ArrayList<String> getPreppedTiles() {
+        return preppedTiles;
+    }
+
     public int getPoints() {
         return points;
+    }
+
+    public boolean getWin() {
+      return win;
     }
 }
