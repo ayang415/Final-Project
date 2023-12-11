273b6782f46f7f4f8e10e5a6d7eb6f12f580b735
ayang415 <148258723+ayang415@users.noreply.github.com>
2023-12-11 11:15:11 -0600

    Update GridView.java

--- GridView.java
+++ GridView.java
@@ -1,10 +1,17 @@
 public class GridView {
-    public void displayGrid(String[][] grid) {
+    public void displayGrid(String[][] grid, Player p) {
         for(String[] row : grid) {
             for(String cell : row) {
                 System.out.print(cell + " ");
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("Hand:");
+        for(Character piece : p.getHand()) {
+            System.out.print(piece + " ");
+        }
+        System.out.println();
+        System.out.println();
     }
 }
