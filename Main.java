16efd41cbc2b74b0564477b9d34fefdc51f4c0e2
ayang415 <148258723+ayang415@users.noreply.github.com>
2023-12-13 11:10:36 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//Store all words into a ArrayList after parsing with BufferList
-
 public class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
@@ -35,52 +33,50 @@ public class Main {
             controller.shuffle();
             controller.draw(p1.getHand(), 7);
             controller.draw(p2.getHand(), 7);
-            //controller.setCell(8, 8, "★");
-            
+
             while(p1.getWin() == false && p2.getWin() == false) {
                 String word, orient;
                 String location = "H8";
                 Player temp = new Player();
                 if(turn % 2 == 1) { temp = p1; }
                 else { temp = p2; }
-              
+
                 System.out.println("\033[H\033[2J");
                 controller.updateView(temp);
-                
+
                 System.out.print("What word do you want to place? ");
                 word = myObj.nextLine().toUpperCase();
-                boolean inHand = controller.inHand(word, temp);
+                /*boolean inHand = controller.inHand(word, temp);
                 while(inHand == false || controller.isWord(word) == false) {
                     if(inHand == true && controller.isWord(word) == false) {
-                            for(int j = 0; j < temp.getPreppedTiles().size(); j++) {
-                                temp.getHand().add(temp.getPreppedTiles().get(j));
-                            }
+                        for(int j = 0; j < temp.getPreppedTiles().size(); j++) {
+                            temp.getHand().add(temp.getPreppedTiles().get(j));
+                        }
+                        temp.getPreppedTiles().clear();
+                        
                     }
                     System.out.print("That word is not in your hand or is not valid. Try again: ");
                     word = myObj.nextLine().toUpperCase();
                     inHand = controller.inHand(word, temp);
-                }
-                
+                }*/
+
 
                 if(turn != 1) {
                     System.out.print("Where do you want to place it (letter + number)? ");
                     location = myObj.nextLine().toUpperCase();
-                    /*while(!controller.isOpen(location)) {
-                        System.out.print("Invalid location. Try again: ");
-                        location = myObj.nextLine().toUpperCase();
-                    }*/
                 }
-          
-                
+
                 System.out.print("Place the word horizontal or vertical (h or v)? ");
                 orient = myObj.nextLine();
                 while(orient.equals("h") == false && orient.equals("v") == false) {
                     System.out.print("Invalid. Try again (h or v): ");
                     orient = myObj.nextLine();
                 }
-                
-                while(controller.spacesCheck(word, location, orient, turn) == false) {
+
+                while(controller.spacesCheck(word, location, orient, temp, turn) == false) {
                     System.out.println("Invalid placement. Try again: ");
+                    System.out.print("What word do you want to place? ");
+                    word = myObj.nextLine().toUpperCase();
                     if(turn != 1) {
                         System.out.print("Where do you want to place it (letter + number)? ");
                         location = myObj.nextLine().toUpperCase();
