a75f12f3f41ab3c69d8039a9984a0d0ab8ba4a3c
ayang415 <148258723+ayang415@users.noreply.github.com>
2023-12-17 17:36:06 -0600

    Final

--- Player.java
+++ Player.java
@@ -3,8 +3,18 @@ import java.util.ArrayList;
 public class Player {
     private ArrayList<Character> hand = new ArrayList<Character>();
     private ArrayList<Character> preppedTiles = new ArrayList<Character>();
-    private boolean win = false;
+    public boolean win = false;
+    public int id = 0;
     public int points = 0;
+    public int addAmt = 0;
+
+    public Player(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public ArrayList<Character> getHand() {
         return hand;
